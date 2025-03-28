5cedb7c266531ddc51ffed1557388c400c1dd901
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-03-28 17:40:26 +0600

    filtercategories filtering done

--- src/serchInp/SearchInput.jsx
+++ src/serchInp/SearchInput.jsx
@@ -5,9 +5,24 @@ const categories = ["electronics", "jewelry", "men's clothing", "women's clothin
 
 const SearchInput = () => {
     const [search, setSearch] = useState('')
+    console.log(search);
+
+
+    const filterCategories = categories.filter((category, index) => category.toLowerCase().includes(search.toLowerCase()))
+    console.log((filterCategories));
+
     return (
         <div>
             <input onChange={(event) => setSearch(event.target.value)} type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+
+
+            <div>
+                <div className='bg-white mt-3'>
+   
+                    {filterCategories.map((category, index) => <p key={index+8796} className='text-black  px-3 p-1 hover:bg-amber-200 '>{category}</p>)}
+                </div>
+            </div>
+
         </div>
     );
 };
