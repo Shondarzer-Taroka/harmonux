449c1574fd68bc2c72e8b7ef7c3eb682fcccfacf
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-03-28 18:47:12 +0600

    search input

--- src/serchInp/SearchInput.jsx
+++ src/serchInp/SearchInput.jsx
@@ -11,16 +11,29 @@ const SearchInput = () => {
     const filterCategories = categories.filter((category, index) => category.toLowerCase().includes(search.toLowerCase()))
     console.log((filterCategories));
 
+
+    const highlightText = (text) => {
+        const regex = new RegExp(`${search}`, 'gi')
+        return text.replace(regex, (match) => `<strong>${match}</strong>`)
+    }
+
     return (
         <div>
             <input onChange={(event) => setSearch(event.target.value)} type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
 
 
             <div>
-                <div className='bg-white mt-3'>
-   
-                    {filterCategories.map((category, index) => <p key={index+8796} className='text-black  px-3 p-1 hover:bg-amber-200 '>{category}</p>)}
-                </div>
+                {search && filterCategories.length > 0 && (
+                    <div className='bg-white mt-3'>
+                        {filterCategories.map((category, index) =>
+                            <p
+                                key={category}
+                                className='text-black  px-3 p-1 hover:bg-amber-200 '
+                                dangerouslySetInnerHTML={{ __html: highlightText(category) }}
+                            />
+                        )}
+                    </div>
+                )}
             </div>
 
         </div>
